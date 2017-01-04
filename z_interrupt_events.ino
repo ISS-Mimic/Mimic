72467785f8ee9d2ed34d0929b6990a5d552bc301
ISS Mimic Project <iss.mimic@gmail.com>
2017-01-04 15:27:33 -0600

    Update z_interrupt_events.ino
    Change to use serial input

--- z_interrupt_events.ino
+++ z_interrupt_events.ino
@@ -20,7 +20,7 @@ void rightEncoderEvent() {
 }
 
 
-void receiveEvent(int howMany)
+/*void receiveEvent(int howMany)
 {
   String test = "";
   while (1 < Wire2.available()) // loop through all but the last
@@ -46,4 +46,91 @@ void receiveEvent(int howMany)
   //Serial.println();
   Serial.println("Inside the Wire Read loop");
   Serial.flush();
+}*/
+void checkSerial()
+{
+  //digitalWrite(ledBluePin, HIGH);
+  test = "";
+  
+  while(Serial1.available())  
+  {
+    test = Serial1.readString();
+  }
+  //Serial.println(test);
+  char sz[test.length() + 1];
+  char copy[test.length() + 1];
+  strcpy(sz, test.c_str());  
+  char *p = sz;
+  char *str;
+  int delimeter = 0;
+  String test2 = ""; 
+  
+  while((str = strtok_r(p," ",&p))!=NULL)
+  {
+    test2 = String(str);
+    delimeter = test2.indexOf('=');  
+    if(test2.substring(0,delimeter)=="PSARJ")
+    {
+      PSARJ = (test2.substring(delimeter+1)).toFloat();
+    }  
+    else if(test2.substring(0,delimeter)=="SSARJ")
+    {
+      SSARJ = (test2.substring(delimeter+1)).toFloat();
+    }  
+    else if(test2.substring(0,delimeter)=="PTRRJ")
+    {
+      PTRRJ = (test2.substring(delimeter+1)).toFloat();
+    } 
+    else if(test2.substring(0,delimeter)=="STRRJ")
+    {
+      STRRJ = (test2.substring(delimeter+1)).toFloat();
+    } 
+    else if(test2.substring(0,delimeter)=="Beta1B")
+    {
+      Beta1B = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta1A")
+    {
+      Beta1A = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta2B")
+    {
+      Beta2B = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta2A")
+    {
+      Beta2A = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta3B")
+    {
+      Beta3B = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta3A")
+    {
+      Beta3A = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta4B")
+    {
+      Beta4B = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="Beta4A")
+    {
+      Beta4A = (test2.substring(delimeter+1)).toFloat();
+    }
+    else if(test2.substring(0,delimeter)=="AOS")
+    {
+      //Serial.println(test2);
+      AOS = (test2.substring(delimeter+1)).toFloat();
+      //Serial.println(AOS);
+      if(AOS == 1.00)
+      {
+        //digitalWrite(ledGreenPin, HIGH);
+      }
+      else
+      {
+        //digitalWrite(ledRedPin, HIGH);
+      }
+    }
+  }
+  
 }
