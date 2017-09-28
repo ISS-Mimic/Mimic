ee2b0b645b88342159d93dcf60c1e0311c3116e3
streadgold <samrtreadgold@gmail.com>
2017-09-28 17:31:43 -0500

    Add files via upload
    This guy can retrieve EVA time for a given crewmember

---
+++ Pi/urltest3.py
@@ -0,0 +1,14 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+nasaissurl = 'http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html'
+#TLE = BeautifulSoup(urllib2.urlopen(nasaissurl), 'html.parser')
+soup = BeautifulSoup(open('./EVA.html'), 'html.parser')
+
+numEVAs = soup.find("td", text="Acaba").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+EVAtime_hours = soup.find("td", text="Acaba").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+EVAtime_minutes = soup.find("td", text="Acaba").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+
+print numEVAs
+print EVAtime_hours
+print EVAtime_minutes
