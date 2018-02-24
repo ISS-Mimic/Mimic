cdb783badf8d2a0dac9cb10b4311f6fe48996f08
ISS-Mimic <samrtreadgold@gmail.com>
2018-02-23 18:23:51 -0600

    adding tle test

---
+++ Pi/againTLE.py
@@ -0,0 +1,13 @@
+import urllib2
+from bs4 import BeautifulSoup
+import ephem
+import datetime
+
+nasaissurl = 'http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html'
+soup = BeautifulSoup(urllib2.urlopen(nasaissurl), 'html.parser')
+body = soup.find_all("pre")
+index = 0
+firstTLE = False
+for tag in body:
+    if "ISS" in tag.text:
+        print tag.text
