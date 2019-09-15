5c6f5adfb970d14b1c5bc7424f12e00fcc294d62
streadgold <samrtreadgold@gmail.com>
2019-09-15 02:18:26 -0500

    Update Test_Kivy.py

--- Pi/kivytest/Test_Kivy.py
+++ Pi/kivytest/Test_Kivy.py
@@ -1,16 +1,6 @@
 import os
 os.environ['KIVY_GL_BACKEND'] = 'gl' #need this to fix a kivy segfault that occurs with python3 for some reason
-import kivy
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.bubble import Bubble
-from kivy.uix.gridlayout import GridLayout
-
-class BubbleButton(Button):
-    pass
-
-class NumPad(Bubble):
-    pass
 
 class TestApp(App):
     pass
