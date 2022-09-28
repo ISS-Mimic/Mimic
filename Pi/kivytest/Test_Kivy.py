bd4d97009fbc7f476ada22bb8dd6e9c8cb75b048
streadgold <samrtreadgold@gmail.com>
2022-09-28 18:07:42 -0500

    Adding stuff to fakeorbit

--- Pi/kivytest/Test_Kivy.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['KIVY_GL_BACKEND'] = 'gl' #need this to fix a kivy segfault that occurs with python3 for some reason
-from kivy.app import App
-
-class TestApp(App):
-    pass
-
-if __name__ == '__main__':
-    TestApp().run()
