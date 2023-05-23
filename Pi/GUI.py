dcd5690aaec2265ce0a9387c303d1681f6476444
ISS Mimic Project <iss.mimic@gmail.com>
2023-05-23 16:18:54 -0500

    Update GUI.py
    trying a temp fix for divide by zero error

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -1877,19 +1877,30 @@ class MainApp(App):
 
             #inclination = ISS_TLE.inc
 
-            #normalizedX = self.orbit_screen.ids.OrbitMap.norm_image_size[0] / self.orbit_screen.ids.OrbitMap.texture_size[0]
-            #normalizedY = self.orbit_screen.ids.OrbitMap.norm_image_size[1] / self.orbit_screen.ids.OrbitMap.texture_size[1]
+            try:
+                normalizedX = self.orbit_screen.ids.OrbitMap.norm_image_size[0] / self.orbit_screen.ids.OrbitMap.texture_size[0]
+            except Exception as e:
+                print(e)
+            else:
+                normalizedX = 0
+                
+            try:
+                normalizedY = self.orbit_screen.ids.OrbitMap.norm_image_size[1] / self.orbit_screen.ids.OrbitMap.texture_size[1]
+            except Exception as e:
+                print(e)
+            else:
+                normalizedY = 0
 
             self.orbit_screen.ids.OrbitISStiny.pos = (
-                    #scaleLatLon2(latitude, longitude)['new_x'] - ((self.orbit_screen.ids.OrbitISStiny.width / 2) * normalizedX * 2), #had to fudge a little not sure why
-                    #scaleLatLon2(latitude, longitude)['new_y'] - ((self.orbit_screen.ids.OrbitISStiny.height / 2) * normalizedY * 2)) #had to fudge a little not sure why
+                    scaleLatLon2(latitude, longitude)['new_x'] - ((self.orbit_screen.ids.OrbitISStiny.width / 2) * normalizedX * 2), #had to fudge a little not sure why
+                    scaleLatLon2(latitude, longitude)['new_y'] - ((self.orbit_screen.ids.OrbitISStiny.height / 2) * normalizedY * 2)) #had to fudge a little not sure why
 
             #get the position of the sub solar point to add the sun icon to the map
             sunlatitude, sunlongitude = subsolar(datetime.utcnow())
 
             self.orbit_screen.ids.OrbitSun.pos = (
-                    #scaleLatLon2(int(sunlatitude), int(sunlongitude))['new_x'] - ((self.orbit_screen.ids.OrbitSun.width / 2) * normalizedX * 2), #had to fudge a little not sure why
-                    #scaleLatLon2(int(sunlatitude), int(sunlongitude))['new_y'] - ((self.orbit_screen.ids.OrbitSun.height / 2) * normalizedY * 2)) #had to fudge a little not sure why
+                    scaleLatLon2(int(sunlatitude), int(sunlongitude))['new_x'] - ((self.orbit_screen.ids.OrbitSun.width / 2) * normalizedX * 2), #had to fudge a little not sure why
+                    scaleLatLon2(int(sunlatitude), int(sunlongitude))['new_y'] - ((self.orbit_screen.ids.OrbitSun.height / 2) * normalizedY * 2)) #had to fudge a little not sure why
 
             #draw the ISS groundtrack behind and ahead of the 180 longitude cutoff
             ISS_groundtrack = []
