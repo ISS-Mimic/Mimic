e261408bab06dfbb9726e692c8d4f0b4d7a461ba
Susan N. Freeman <33506403+GodspeedDiscvry@users.noreply.github.com>
2023-05-23 20:57:46 -0500

    Adding crew screen to list of screens with signal

--- Pi/GUI.py
+++ Pi/GUI.py
@@ -1282,7 +1282,7 @@ class MainApp(App):
         ScreenList = ['tcs_screen', 'eps_screen', 'iss_screen', 'eclss_screen',
                       'ct_screen', 'ct_sasa_screen', 'ct_sgant_screen', 'ct_uhf_screen',
                       'ct_camera_screen', 'gnc_screen', 'orbit_screen', 'us_eva', 'rs_eva',
-                      'eva_main', 'mimic_screen', 'mss_mt_screen','orbit_pass','orbit_data']
+                      'eva_main', 'mimic_screen', 'mss_mt_screen','orbit_pass','orbit_data','crew_screen']
 
         root = MainScreenManager(transition=SwapTransition())
         root.add_widget(self.main_screen)
