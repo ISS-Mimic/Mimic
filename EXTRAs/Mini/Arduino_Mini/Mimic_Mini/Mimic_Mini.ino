16da0a1a204158ff2860ed8acafdc11b54816fc1
Juggler2000 <bryan.c.murphy@gmail.com>
2023-11-12 17:38:25 -0600

    Added Mini Mimic Ard files that were accidently deleted

---
+++ EXTRAs/Mini/Arduino_Mini/Mimic_Mini/Mimic_Mini.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+
+// Depending on your servo make, the pulse width min and max may vary, you
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  90//82 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  500//482 // This is the 'maximum' pulse length count (out of 4096)
+//#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+//#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+// our servo # counter
+uint8_t servonum = 0;
+
+
+//int D0 = 0;
+//int D1 = 0;
+//int D2 = 0;
+int MimiSetAllServos_Flag=0;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  /*
+     In theory the internal oscillator (clock) is 25MHz but it really isn't
+     that precise. You can 'calibrate' this by tweaking this number until
+     you get the PWM update frequency you're expecting!
+     The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+     is used for calculating things like writeMicroseconds()
+     Analog servos run at ~50 Hz updates, It is importaint to use an
+     oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+     1) Attach the oscilloscope to one of the PWM signal pins and ground on
+        the I2C PCA9685 chip you are setting the value for.
+     2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+        expected value (50Hz for most ESCs)
+     Setting the value here is specific to each individual I2C PCA9685 chip and
+     affects the calculations for the PWM update frequency.
+     Failure to correctly set the int.osc value will cause unexpected PWM results
+  */
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
+}
