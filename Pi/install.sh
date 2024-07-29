f3ede15878d4455ea74c4cc29a4c73f7a6f599ac
ISS Mimic Project <iss.mimic@gmail.com>
2024-07-28 22:49:08 -0500

    Delete Pi/install.sh

--- Pi/install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install vim
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install git-all
-git clone https://github.com/ISS-Mimic/Mimic.git
