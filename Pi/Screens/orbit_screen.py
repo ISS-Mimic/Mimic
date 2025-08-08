58576a9c97e23acdbc0af47dfc375c8c691fff90
streadgold <iss.mimic@gmail.com>
2025-08-08 12:32:00 -0500

    dot works finally, fixing tdrs active update

--- Pi/Screens/orbit_screen.py
+++ Pi/Screens/orbit_screen.py
@@ -83,6 +83,7 @@ class Orbit_Screen(MimicBase):
         Clock.schedule_interval(self.update_globe,        31)
         Clock.schedule_interval(self.update_tdrs,        607)
         Clock.schedule_interval(self.update_sun,         489) 
+        Clock.schedule_interval(self.update_active_tdrs,   1)
 
         # one-shots that existed in MainApp.build()
         Clock.schedule_once(self.update_iss_tle,          60)
